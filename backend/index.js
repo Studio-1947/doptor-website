cd20497de3c0dd15a6c11dc526642e40d2b8c78a
soumic sarkar <soumic@codillion.com>
2026-01-20 23:13:36 +0530

    another

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-import allowCors from "./lib/cors.js";
-
-const handler = async (req, res) => {
-  res.status(200).send("Doptor Backend Service is Running");
-};
-
-export default allowCors(handler);
