58428c7f32c4960e9923c2843307f3d817dfd622
soumic sarkar <soumic@codillion.com>
2026-01-21 10:55:23 +0530

    feat: Implement Prisma ORM with initial database schema and client setup.

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -3,10 +3,22 @@ import { PrismaClient } from "@prisma/client";
 let prisma;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
   }
   prisma = global.prisma;
 }
